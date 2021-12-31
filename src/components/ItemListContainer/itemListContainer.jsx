044725ce919d66cc041af0c987c1befc9cafec64
Pol <rivarolapol@gmail.com>
2021-12-30 23:24:43 -0300

    entrega final 2

--- src/components/ItemListContainer/itemListContainer.jsx
+++ src/components/ItemListContainer/itemListContainer.jsx
@@ -3,36 +3,28 @@ import ItemList from "../ItemList/ItemList";
 import { useState } from "react/cjs/react.development";
 import { useParams } from "react-router";
 import { useEffect } from "react";
-import { usePelis, useIsLoading } from "../../context/contextPelis";
 import {
   getFirestore,
   collection,
   getDocs,
   query,
   where,
-  updateDoc,
-  deleteDoc,
 } from "firebase/firestore";
 
 const ItemListContainer = (props) => {
-  const [items, setItems] = useState([]);
   let { catID } = useParams();
   const [pelis, setPelis] = useState([]);
   const [isLoading, setIsLoading] = useState(true);
   const dataBase = getFirestore();
   const ref = collection(dataBase, "peliculas");
   useEffect(() => {
-
-    const pelisShow = catID? query(ref,where("category","==",catID))
-                      :ref
+    const pelisShow = catID ? query(ref, where("category", "==", catID)) : ref;
 
     getDocs(pelisShow)
-
       .then((snapShot) => {
         setPelis(
           snapShot.docs.map((peli) => ({ id: peli.id, ...peli.data() }))
         );
-        
       })
       .catch((error) => {
         console.log(error);
@@ -55,6 +47,5 @@ const ItemListContainer = (props) => {
     </section>
   );
 };
-
 
 export default ItemListContainer;
